2e10f4f5320a1c6c32fc657b0d830703a14d8150
Vladyslav <vladyslav@euot.com>
2020-02-07 13:11:44 +0200

    add classes

--- src/com/courses/Main.java
+++ src/com/courses/Main.java
@@ -1,8 +1,13 @@
 package com.courses;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+
 
     }
+
 }
